013a391a4344342a68e2278a4d0bf381ff8b46ad
river-bank <k.bancroft@qmul.ac.uk>
2022-11-17 16:54:30 +0000

    spilt out providers into separate file

--- createinstance.tf
+++ createinstance.tf
@@ -1,10 +1,4 @@
-# Configure the AWS Provider
-provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAYYAA3ZDLBNBQYBCE"
-    secret_key = "SECRET_KEY"
-    region     = "eu-west-2"
-}
+
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0b64c3034d0bcd78a"
---
+++ provider.tf
@@ -0,0 +1,7 @@
+# Configure the AWS Provider
+provider "aws" {
+    version = "3.53.0"
+    access_key = "AKIAYYAA3ZDLONP7KBSZ"
+    secret_key = ""
+    region     = "eu-west-2"
+}
