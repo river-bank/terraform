ea7296f11f15ec95aa21c580a1020b56f0d15f07
river-bank <k.bancroft@qmul.ac.uk>
2022-11-17 22:40:41 +0000

    edit01

--- terrorform-vars/variables.tf
+++ terrorform-vars/variables.tf
@@ -3,3 +3,9 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_ACCESS_REGION" {
 default = "eu-west-2"
 }
+
+variable "Security_Group" {
+    type = "list"
+    default = ["sg-12345", "sg-54234", "sg-456782"]
+  
+}
