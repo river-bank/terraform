a058609f310d4d8f802f77fc2a0c373dbf0795ec
river-bank <k.bancroft@qmul.ac.uk>
2022-11-17 23:06:50 +0000

    var-name-error

--- terrorform-vars/createinstance.tf
+++ terrorform-vars/createinstance.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+  ami           = lookup(var.AMIS, var.AWS_ACCESS_REGION)
   instance_type = "t2.micro"
 
   security_groups = "${var.Security_Group}"
