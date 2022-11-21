8b1726804c83cd96ef19de7a5cb49ffa11b00321
river-bank <k.bancroft@qmul.ac.uk>
2022-11-21 20:43:27 +0000

    done

--- demo-datasource/securitygroup.tf
+++ demo-datasource/securitygroup.tf
@@ -11,7 +11,8 @@ resource "aws_security_group" "sa-custom_eu_west" {
       from_port = "443"
       to_port =   "443"
       protocol =  "tcp"  
-      cidr_blocks = data.aws_ip_ranges.eu_west_ip_range.cidr_blocks 
+      # cidr_blocks = data.aws_ip_ranges.eu_west_ip_range.cidr_blocks 
+      cidr_blocks = ["0.0.0.0/0"]
     } 
 
     tags = {
