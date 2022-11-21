7e4f925c6a25a0fc932daf0a9c9e593d96a11f95
river-bank <k.bancroft@qmul.ac.uk>
2022-11-21 15:57:23 +0000

    zones

--- demo-datasource/createInstance.tf
+++ demo-datasource/createInstance.tf
@@ -1,9 +1,9 @@
-data "aws_availability_zone" "avilable" {}
+data "aws_availability_zones" "avilable" {}
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zone.avilable.name[1]
+  availability_zone = data.aws_availability_zones.avilable.names[1]
   tags = {
     Name = "custom_instance"
   }
